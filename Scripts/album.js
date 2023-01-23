076f9f1ac06375143fd9e5607f6048d88163a36e
Batı Gökovalı <batigokovali@icloud.com>
2023-01-23 19:39:51 +0300

    Update

--- Scripts/album.js
+++ Scripts/album.js
@@ -1,5 +1,6 @@
 function changeNavbarBG() {
     let navbar = document.getElementById("album-navbar")
+    let navbar2 = document.getElementById("top-navbar")
     let scrollValue = window.scrollY;
     if (scrollValue < 510) {
         navbar.classList.remove("album-navbar-bg")
