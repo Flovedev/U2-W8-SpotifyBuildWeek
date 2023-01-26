7bc8d68b8de3f71c61435a85daebfbf9faf8eed2
Flovedev <flove.dev@gmail.com>
2023-01-26 10:23:01 +0000

    css updates

--- js/layout.js
+++ js/layout.js
@@ -20,41 +20,41 @@ window.onload = async () => {
         if (res.ok) {
             console.log(resJson)
             artistNode.innerHTML = `
-            <div class="jumbotron pb-1 pl-4 d-flex flex-column" id="artist-jumbotron" style="background-image: url('${resJson.picture_big}')">
+            <div class="jumbotron pb-1 pl-4 mb-0 d-flex flex-column" id="artist-jumbotron" style="background-image: url('${resJson.picture_xl}')">
             <div class="mt-auto">
                 <div class="d-flex align-items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="lightblue"
-                        class="bi bi-check-circle-fill" viewBox="0 0 16 16">
-                        <path
-                            d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#3D91F4" class="bi bi-patch-check-fill" viewBox="0 0 16 16">
+                        <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z"/>
                     </svg>
                     <p class="m-1">Verified Artist</p>
                 </div>
             </div>
             <h1 class="display-4">${resJson.name}</h1>
-            <p class="lead">37,120,733 monthly listeners</p>
+            <p class="lead">A lot of monthly listeners</p>
             </div>
             <div id="artist-content">
-            <div class="m-4">
+            <div class="p-4">
                 <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="green"
-                    class="bi bi-play-circle-fill" viewBox="0 0 16 16">
+                    class="bi bi-play-circle-fill mr-4" viewBox="0 0 16 16">
                     <path
                         d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
                 </svg>
-                <button type="button" class="btn btn-outline-light">Follow</button>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-three-dots"
+                <button type="button" class="btn btn-outline-light mr-4">Follow</button>
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#808080" class="bi bi-three-dots"
                     viewBox="0 0 16 16">
                     <path
                         d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
                 </svg>
             </div>
-            <div class="d-flex m-4">
+            <div class="d-flex p-4">
                 <div class="flex-grow-1">
                     <h5>Popular</h5>
                     <table class="table table-borderless m-3 table-dark bg-transparent">
-                        <tbody id="artist-tracks"></tbody>
+                        <tbody class="" id="artist-tracks"></tbody>
                     </table>
-                    <span>SEE MORE</span>
+                    <div id="artist-content-see" onclick="">
+                        <span>SEE MORE</span>
+                    </div>
                 </div>
                 <div class="mx-5 pr-5" id="artist-pick">
                     <h5>Artist pick</h5>
@@ -111,11 +111,10 @@ const getTracklist = async () => {
     }
 }
 
-
 const searchArtist = (event) => {
     const input = event.target.value
     if (event.key === "Enter") {
-        console.log(`I Have to render ${input}`)
+
     }
 }
 
